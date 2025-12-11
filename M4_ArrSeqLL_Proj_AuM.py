2f0a885f91348a66317dfdfa8f970a512451c675
me <yuh@bruh.com>
2025-12-11 02:30:50 -0500

    implemented singly linked list program

--- M4_ArrSeqLL_Proj_AuM.py
+++ M4_ArrSeqLL_Proj_AuM.py
@@ -1,6 +1,7 @@
 from stack import arrayStack
 from postfix_eval import postfixEval
 from infix_conv import in2postfixConv
+from singly_linked_list import SinglyLinkedList
 
 if __name__=="__main__":
     converter = in2postfixConv()
@@ -33,3 +34,68 @@ if __name__=="__main__":
             print(f"{infix:<30} | {pfix}")
         except Exception as e:
             print(f"{infix:<30} | Error: {e}")
+
+
+print("----Building forward list----\n")
+sll_fwd = SinglyLinkedList()
+sll_fwd.build_forward_list([10,20,30,40,50])
+sll_fwd.display()
+
+print(">>>>Deleting first node: ", end="")
+sll_fwd.remove(10)
+sll_fwd.display()
+
+print(">>>>Deleting last node: ", end="")
+sll_fwd.remove(50)
+sll_fwd.display()
+
+print(">>>>Deleting interior node: ", end="")
+sll_fwd.remove(30)
+sll_fwd.display()
+print("\n----------------------------\n")
+
+print("----Building backwards----\n")
+sll_back = SinglyLinkedList()
+sll_back.build_backward_list([10,20,30,40,50])
+sll_back.display()
+
+print(">>>>Deleting first node: ", end="")
+sll_back.remove(50)
+sll_back.display()
+
+print(">>>>Deleting last node: ", end="")
+sll_back.remove(10)
+sll_back.display()
+
+print(">>>>Deleting interior node: ", end="")
+sll_back.remove(30)
+sll_back.display()
+print("\n----------------------------\n")
+
+print("----Nonrecursive reverse printing----\n")
+sll_rev = SinglyLinkedList()
+sll_rev.build_forward_list([1,2,3,4,5])
+
+print("\n>>>>Insertion order: ", end="")
+sll_rev.display()
+
+print("\n>>>>Reverse order (recursive): ", end="")
+sll_rev.display_reverse()
+
+print("\n>>>>Reverse (nonrecursive): ", end="")
+sll_rev.display_reverse_nr()
+print("\n----------------------------\n")
+
+print("----Remove test----\n")
+sll_remall = SinglyLinkedList()
+sll_remall.build_forward_list([1,3,7,3,4,8,1])
+sll_remall.display()
+
+print(">>>>Removing 1+dupes\n")
+sll_remall.remove_all(1)
+sll_remall.display()
+
+print("\n>>>>Removing 7+dupes\n")
+sll_remall.remove_all(7)
+sll_remall.display()
+print("\n----Singly Linked List Test Done----\n")
--- singly_linked_list.py
+++ singly_linked_list.py
@@ -3,36 +3,47 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
+
 class SinglyLinkedList:
     class EmptyListException(Exception):
         pass
+
     class NodeNotFoundException(Exception):
         pass
+
     def __init__(self):
         self.__head = None
         self.__tail = None
         self.__count = 0
+
     def build_forward_list(self, iterable):
         for item in iterable:
             self.__append(item)
+
     def build_backward_list(self, iterable):
         for item in iterable:
             self.__prepend(item)
+
     def __append(self, value):
+
         new_node = Node(value)
+
         if self.__head is None:
             self.__head = self.__tail = new_node
+
         else:
             self.__tail.next = new_node
             self.__tail = new_node
             self.__count += 1
-        def __prepend(self, value):
-            new_node = Node(value)
-            new_node.next = self.__head
-            self.__head = new_node
-            if self.__tail is None:
-                self.__tail = new_node
-            self.__count += 1
+
+    def __prepend(self, value):
+        new_node = Node(value)
+        new_node.next = self.__head
+        self.__head = new_node
+        if self.__tail is None:
+            self.__tail = new_node
+        self.__count += 1
+
     def insert_after(self, after_value, new_value):
         if self.__head is None:
             raise SinglyLinkedList.EmptyListException()
@@ -41,12 +52,14 @@ class SinglyLinkedList:
             current = current.next
         if current is None:
             raise SinglyLinkedList.NodeNotFoundException()
+
         new_node = Node(new_value)
         new_node.next = current.next
         current.next = new_node
         if current == self.__tail: #if inserted at end of list
             self.__tail = new_node #re-route the tail pointer
         self.__count += 1
+
     def remove(self, value):
         if self.__head is None:
             raise SinglyLinkedList.EmptyListException()
@@ -64,6 +77,32 @@ class SinglyLinkedList:
         if current == self.__tail:
             self.__tail = previous #remove the last node
             self.__count -= 1
+
+    def remove_all(self, value):
+        if self.__head is None:
+            return
+
+        while self.__head and self.__head.data == value:
+            self.__head = self.__head.next
+            self.__count -= 1
+        
+        if self.__head is None:
+            self.__tail = None
+            return
+
+        current = self.__head
+        while current.next:
+            if current.next.data == value:
+
+                current.next = current.next.next
+                self.__count -= 1
+
+                if current.next is None:
+                    self.__tail = current
+            else:
+
+                current = current.next
+
     def display(self):
         current = self.__head
         elements = []
@@ -71,14 +110,33 @@ class SinglyLinkedList:
             elements.append(str(current.data))
             current = current.next
         print(" -> ".join(elements))
+
     def display_reverse(self):
         def _reverse_recursive(node):
             return _reverse_recursive(node.next) + [node.data] if node else []
         print(" <- ".join(map(str,_reverse_recursive(self.__head))))
+
+    def display_reverse_nr(self): #nonrecursive
+        stack = []
+        current = self.__head
+        
+        while current:
+            stack.append(current.data)
+            current = current.next
+            
+        output_parts = ["None"]
+        while stack:
+            output_parts.append(str(stack.pop()))
+        output_parts.append("Head")
+        
+        print(" <- ".join(output_parts))
+
     def __iter__(self):
         current = self.__head
         while current:
             yield current.data
             current = current.next
+
     def __len__(self):
         return self.__count
+    
