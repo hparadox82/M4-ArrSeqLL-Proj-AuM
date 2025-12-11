4fc8390ac4938b7b03451a21daac7abdd934a88d
me <yuh@bruh.com>
2025-12-11 03:25:15 -0500

    removed prev progs

--- M4_ArrSeqLL_Proj_AuM.py
+++ M4_ArrSeqLL_Proj_AuM.py
@@ -5,119 +5,23 @@ from singly_linked_list import SinglyLinkedList
 from split_odds_evens import SplitEO
 
 if __name__=="__main__":
-    converter = in2postfixConv()
-    evaluator = postfixEval()
 
-    print("---Infix>Postfix Test Program---")
-    
+    print("----Split Even Odd Test----\n    |Building list...\n")
+    split_list=SplitEO()
+    data2 = [11, 25, 32, 42, 57, 64, 79]
+    split_list.build_forward_list(data2)
 
-    ##setting postfix list as per project instructions
-    pftest1 = ["5 3 +","8 2 - 3 +","5 3 8 * +","6 2 / 3 +","5 8 + 3 -","5 3 + 8 *","8 2 3 * + 6 -","5 3 8 * + 2 /","8 2 + 3 6 * -","5 3 + 8 2 / -"]
-    print("Testing postfix evaluation...\n")
-    print(f"{'Expression':<20} | {'Result'}")
-    print("-" * 30)
+    print(">>>>Original list: ")
+    split_list.display()
 
-    for expr in pftest1:
-        try:
-            result = evaluator.evaluate(expr)
-            print(f"{expr:<20} | {result}")
-        except Exception as e:
-            print(f"{expr:<20} | Error: {e}")
+    print("\n>>>>Splitting....")
+    evens, odds = split_list.spleo()
 
-    i2ptest1 = ["A + B","A + B * C","( A + B ) * C","A * B + C / D","( A + B ) * ( C - D )","A + B * C - D / E","A * ( B + C ) / D","( A + B * C ) / ( D - E )","A + ( B - C ) * D","( A + B * ( C - D ) ) / E"]
-    print("Testing infix to postfix converter...\n")
-    print(f"{'Infix Expression':<30} | {'Postfix Result'}")
-    print("-" * 60)
+    print("\n>>>>Evens: \n")
+    evens.display()
 
-    for infix in i2ptest1:
-        try:
-            pfix = converter.convert(infix)
-            print(f"{infix:<30} | {pfix}")
-        except Exception as e:
-            print(f"{infix:<30} | Error: {e}")
+    print("\n>>>>Odds: \n")
+    odds.display()
 
-
-print("----Building forward list----\n")
-sll_fwd = SinglyLinkedList()
-sll_fwd.build_forward_list([10,20,30,40,50])
-sll_fwd.display()
-
-print(">>>>Deleting first node: ", end="")
-sll_fwd.remove(10)
-sll_fwd.display()
-
-print(">>>>Deleting last node: ", end="")
-sll_fwd.remove(50)
-sll_fwd.display()
-
-print(">>>>Deleting interior node: ", end="")
-sll_fwd.remove(30)
-sll_fwd.display()
-print("\n----------------------------\n")
-
-print("----Building backwards----\n")
-sll_back = SinglyLinkedList()
-sll_back.build_backward_list([10,20,30,40,50])
-sll_back.display()
-
-print(">>>>Deleting first node: ", end="")
-sll_back.remove(50)
-sll_back.display()
-
-print(">>>>Deleting last node: ", end="")
-sll_back.remove(10)
-sll_back.display()
-
-print(">>>>Deleting interior node: ", end="")
-sll_back.remove(30)
-sll_back.display()
-print("\n----------------------------\n")
-
-print("----Nonrecursive reverse printing----\n")
-sll_rev = SinglyLinkedList()
-sll_rev.build_forward_list([1,2,3,4,5])
-
-print("\n>>>>Insertion order: ", end="")
-sll_rev.display()
-
-print("\n>>>>Reverse order (recursive): ", end="")
-sll_rev.display_reverse()
-
-print("\n>>>>Reverse (nonrecursive): ", end="")
-sll_rev.display_reverse_nr()
-print("\n----------------------------\n")
-
-print("----Remove test----\n")
-sll_remall = SinglyLinkedList()
-sll_remall.build_forward_list([1,3,7,3,4,8,1])
-sll_remall.display()
-
-print(">>>>Removing 1+dupes\n")
-sll_remall.remove_all(1)
-sll_remall.display()
-
-print("\n>>>>Removing 7+dupes\n")
-sll_remall.remove_all(7)
-sll_remall.display()
-print("\n   |Singly linked list test done!\n")
-print("\n----------------------------\n")
-
-print("----Split Even Odd Test----\n    |Building list...\n")
-split_list=SplitEO()
-data2 = [11, 25, 32, 42, 57, 64, 79]
-split_list.build_forward_list(data2)
-
-print(">>>>Original list: ")
-split_list.display()
-
-print("\n>>>>Splitting....")
-evens, odds = split_list.spleo()
-
-print("\n>>>>Evens: \n")
-evens.display()
-
-print("\n>>>>Odds: \n")
-odds.display()
-
-print("\n>>>>Verifying that list is empty....\n     |If nothing is displayed, then the list is empty!")
-split_list.display()
+    print("\n>>>>Verifying that list is empty....\n     |If nothing is displayed, then the list is empty!")
+    split_list.display()
